0117c064adb56a3953aa5b371ecde5ca18fe0b87
Asusot <55901295+Asusot@users.noreply.github.com>
2019-12-06 21:49:13 -0300

    Update cadastro_get.php

--- cadastro_get.php
+++ cadastro_get.php
@@ -6,11 +6,11 @@ $email=$_GET["email"];
 
 $telefone=$_GET["telefone"];
 
-$conexao=mysqli_connect("fdb20.awardspace.net","3138530_guriba","159753guriba","3138530_guriba");
+$conexao=mysqli_connect("fdb25.awardspace.net","3250829_finalizandosop","0804rosa","3250829_finalizandosop");
 if (!$conexao)
 	die ("Ocorreu o seguinte erro -> ".msqli_error());
 
-$query="INSERT INTO `3138530_guriba`.`formulario` (`nome`,`email`, `telefone`, `id`) VALUES ('".$nome."','".$email."', '".$telefone."', default)";
+$query="INSERT INTO `3250829_finalizandosop`.`asuost` (`nome`,`email`, `telefone`, `id`) VALUES ('".$nome."','".$email."', '".$telefone."', default)";
 
 mysqli_query($conexao,$query);
 
